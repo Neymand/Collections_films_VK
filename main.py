0a135e442fb00d094abb59b798e72bd80c98b023
Neymand <neymand5@gmail.com>
2025-05-20 22:25:52 +0400

    Добавлены классы Movie и MovieCollection

---
+++ main.py
@@ -0,0 +1,27 @@
+
+class Movie:
+
+    def __init__(self, title: str, year: int, genre: str):
+        self.title = title
+        self.year = year
+        self.genre = genre
+
+class MovieCollection:
+
+    def __init__(self):
+        pass
+
+    def add_movie(self):
+        pass
+
+    def remove_movie(self):
+        pass
+
+    def search_movies(self):
+        pass
+
+    def add_movie_collection(self):
+        pass
+
+    def remove_movie_from_collection(self):
+        pass
